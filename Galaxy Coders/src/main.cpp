8efb8fe53f60a0dca23efa7e334e8b4df08b77a1
Alex Georgiev <aggeorgiev22@codingburgas.bg>
2024-02-26 06:07:56 +0200

    Add Information Text

--- Galaxy Coders/src/main.cpp
+++ Galaxy Coders/src/main.cpp
@@ -15,13 +15,24 @@ int main() {
 
 
 
-	//Game Text
+	//GAME TEXT
 	sf::Text StartGameText;
 	sf::Text KeyPressText;
 	sf::Text Score;
 	sf::Text ScoreText;
 	sf::Text ResetText;
 	sf::Text DyingText;
+	sf::Text DyingResetText;
+	sf::Text Info1;
+	sf::Text Info2;
+	sf::Text Info3;
+	sf::Text Info4;
+	sf::Text Info5;
+	sf::Text Info6;
+	sf::Text Info7;
+	sf::Text Info8;
+	sf::Text Info9;
+	sf::Text Info10;
 
 	sf::Font StartFont;
 	sf::Font KGCPFont;
@@ -61,7 +72,6 @@ int main() {
 	ScoreText.setCharacterSize(70);
 	ScoreText.setFillColor(sf::Color::White);
 
-
 	ResetText.setString("Press R To Reset");
 	ResetText.setFont(KGCPFont);
 	ResetText.setPosition(sf::Vector2f(600, 800));
@@ -82,14 +92,79 @@ int main() {
 
 	DyingText.setString("YOU DIED!");
 	DyingText.setFont(KGCPFont);
-	DyingText.setPosition(sf::Vector2f(680, 280));
-	DyingText.setCharacterSize(60);
+	DyingText.setPosition(sf::Vector2f(670, 290));
+	DyingText.setCharacterSize(65);
 	DyingText.setFillColor(sf::Color::Black);
+
+	DyingResetText.setString("Press Space To Reset");
+	DyingResetText.setFont(KGCPFont);
+	DyingResetText.setPosition(sf::Vector2f(550, 800));
+	DyingResetText.setCharacterSize(50);
+	DyingResetText.setFillColor(sf::Color::White);
+
+	Info1.setString("The Higgs boson is an elementary particle predicted\n in the standard model of particle physics.");
+	Info1.setFont(KGCPFont);
+	Info1.setPosition(sf::Vector2f(360, 390));
+	Info1.setCharacterSize(35);
+	Info1.setFillColor(sf::Color::Black);
+
+	Info2.setString("The Higgs boson's existence was predicted in 1964 by \nPeter Higgs, Francois Englert, Robert Brout, and \nother scientists.");
+	Info2.setFont(KGCPFont);
+	Info2.setPosition(sf::Vector2f(360, 390));
+	Info2.setCharacterSize(35);
+	Info2.setFillColor(sf::Color::Black);
+
+	Info3.setString("The Higgs boson was discovered in experiments at \nthe Large Hadron Collider in 2012.");
+	Info3.setFont(KGCPFont);
+	Info3.setPosition(sf::Vector2f(360, 390));
+	Info3.setCharacterSize(35);
+	Info3.setFillColor(sf::Color::Black);
+
+	Info4.setString("The Higgs boson is also known as the God particle \ndue to its crucial role in the structure of the \nUniverse.");
+	Info4.setFont(KGCPFont);
+	Info4.setPosition(sf::Vector2f(380, 390));
+	Info4.setCharacterSize(35);
+	Info4.setFillColor(sf::Color::Black);
+
+	Info5.setString("The mass of the Higgs boson is about 125 \ngigaelectronvolts, with a spin of 0 and an \nelectric charge of 0.");
+	Info5.setFont(KGCPFont);
+	Info5.setPosition(sf::Vector2f(450, 390));
+	Info5.setCharacterSize(35);
+	Info5.setFillColor(sf::Color::Black);
+
+	Info6.setString("The Higgs boson plays a crucial role in the \nmechanism of assigning mass to other particles, \nincluding hadrons and leptons.");
+	Info6.setFont(KGCPFont);
+	Info6.setPosition(sf::Vector2f(380, 390));
+	Info6.setCharacterSize(35);
+	Info6.setFillColor(sf::Color::Black);
+
+	Info7.setString("The Higgs boson is part of the quantum field \nknown as the Higgs field, which fills the entire \nUniverse.");
+	Info7.setFont(KGCPFont);
+	Info7.setPosition(sf::Vector2f(380, 390));
+	Info7.setCharacterSize(37);
+	Info7.setFillColor(sf::Color::Black);
+
+	Info8.setString("The Higgs boson can decay into other particles, \nallowing scientists to study its properties and \ninteractions.");
+	Info8.setFont(KGCPFont);
+	Info8.setPosition(sf::Vector2f(390, 390));
+	Info8.setCharacterSize(35);
+	Info8.setFillColor(sf::Color::Black);
+
+	Info9.setString("The discovery of the Higgs boson helps us better \nunderstand the origin of the Universe and its early \nstages of development.");
+	Info9.setFont(KGCPFont);
+	Info9.setPosition(sf::Vector2f(360, 390));
+	Info9.setCharacterSize(35);
+	Info9.setFillColor(sf::Color::Black);
+
+	Info10.setString("Understanding the Higgs boson opens up new perspectives for \nscientific research and technological developments, including \nthe creation of new methods for disease treatment and the \n                        development of new materials.");
+	Info10.setFont(KGCPFont);
+	Info10.setPosition(sf::Vector2f(360, 390));
+	Info10.setCharacterSize(29);
+	Info10.setFillColor(sf::Color::Black);
 
 
 
 	//BACKGROUND TEXTURES
-
 	sf::Texture mntnBackground;
 	if (!mntnBackground.loadFromFile("assets/45908.jpg")) {
 		std::cout << "Couldn't load mountain background" << std::endl;
@@ -200,6 +275,7 @@ int main() {
 	RocketSprite2.setScale(sf::Vector2f(1, 1));
 	RocketSprite2.setOrigin(sf::Vector2f(100, 0));
 	float xRocketVelocity = 10;
+	float yRocketVelocity = 12;
 
 	sf::Clock ScoreTimer;
 
@@ -216,28 +292,32 @@ int main() {
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 			RocketPosition2.x += xRocketVelocity;
-			
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+			if (RocketPosition2.y > 0) {
+				RocketPosition2.y -= yRocketVelocity;
+			}
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+			if (RocketPosition2.y < 700) {
+				RocketPosition2.y += yRocketVelocity;
+			}
 		}
 		RocketSprite2.setPosition(RocketPosition2);
 
 
 
 		//Walls Collision Physics
-		if (RocketPosition2.x > 1840) {
-			RocketPosition2.x = -10;
+		if (RocketPosition2.x > 1600) {
+			RocketPosition2.x = -90;
 		}
 
-		else if (RocketPosition2.x < -10) {
-			RocketPosition2.x = 1840;
+		else if (RocketPosition2.x < -100) {
+			RocketPosition2.x = 1600;
 		}
 
-		else if (RocketPosition2.x > 1840) {
-			RocketPosition2.x = -10;
-		}
-
-		else if (RocketPosition2.x < -10) {
-			RocketPosition2.x = 1840;
-		}
 
 		RocketSprite2.setPosition(RocketPosition2);
 
@@ -275,14 +355,19 @@ int main() {
 						GameOn = false;
 						mntnPosition.y = 0;
 						mntnSprite.setPosition(mntnPosition);
-						RocketPosition.x = 810;
+						RocketPosition2.x = 810;
 						skyPosition.y = -900;
 						skyPosition2.y = -1800;
 						spacePosition.y = -900;
+						SpaceSprite.setPosition(spacePosition);
 						spacePosition2.y = -1800;
+						SpaceSprite2.setPosition(spacePosition2);
 						SkySpriteLayers = 0;
 						yBackgroundVel = 2;
 						transitionPosition.y = -900;
+						TransitionSprite.setPosition(transitionPosition);
+						M1Position.y = -140;
+						M1Sprite.setPosition(M1Position);
 					}
 
 					else {
@@ -296,14 +381,17 @@ int main() {
 						GameOn = false;
 						mntnPosition.y = 0;
 						mntnSprite.setPosition(mntnPosition);
-						RocketPosition.x = 810;
+						RocketPosition2.x = 810;
 						skyPosition.y = -900;
 						skyPosition2.y = -1800;
 						spacePosition.y = -900;
+						SpaceSprite.setPosition(spacePosition);
 						spacePosition2.y = -1800;
+						SpaceSprite2.setPosition(spacePosition2);
 						SkySpriteLayers = 0;
 						yBackgroundVel = 2;
 						transitionPosition.y = -900;
+						TransitionSprite.setPosition(transitionPosition);
 						M1Position.y = -140;
 						M1Sprite.setPosition(M1Position);
 					}
@@ -335,7 +423,7 @@ int main() {
 
 
 
-			//Background Movement
+			//BACKGROUND MOVEMENT
 			if (yBackgroundVel < 15) {
 				yBackgroundVel += 0.1f;
 			}
@@ -343,8 +431,8 @@ int main() {
 			mntnPosition.y += yBackgroundVel;
 			mntnSprite.setPosition(mntnPosition);
 
-			//Background Loop
-			if (SkySpriteLayers <= 10) {
+			//Background Loops
+			if (SkySpriteLayers <= 50) {
 				
 				if (skyPosition.y >= 890) {
 					skyPosition.y = -900;
@@ -359,7 +447,7 @@ int main() {
 					SkySpriteLayers += 2;
 				}
 			}
-			if (SkySpriteLayers >= 12 && SkySpriteLayers < 14) {
+			if (SkySpriteLayers >= 52 && SkySpriteLayers < 54) {
 				
 				transitionPosition.y += yBackgroundVel;
 				TransitionSprite.setPosition(transitionPosition);
@@ -370,7 +458,7 @@ int main() {
 				}
 			}
 
-			if (SkySpriteLayers >= 12) {
+			if (SkySpriteLayers >= 52) {
 
 				if (spacePosition.y >= 890) {
 					spacePosition.y = -900;
@@ -418,28 +506,28 @@ int main() {
 					int MeteoriteAngle = (rand() % 8);
 					MeteoriteAngleVel = MeteoriteAngle;
 					if (MeteoriteAngleVel <= 2) {
-						MeteoriteAngleVel += 3;
+						MeteoriteAngleVel += 5;
 					}
 				}
 				else {
 					int MeteoriteAngle = (rand() % 8) - 16;
 					MeteoriteAngleVel = MeteoriteAngle;
 					if (MeteoriteAngleVel >= -2) {
-						MeteoriteAngleVel -= 3;
+						MeteoriteAngleVel -= 5;
 					}
 				}
 			}
 
 			//Meteorite Movement
 			if (SkySpriteLayers >= 2) {
-				M1Position.y += 10.0f;
+				M1Position.y += 15.0f;
 				M1Position.x += MeteoriteAngleVel;
 	
 				M1Sprite.setPosition(M1Position);
 				window.draw(M1Sprite);
 
 				if (M1Position.y >= 890) {
-					M1Position.y = -150;
+					M1Position.y = -130;
 					M1Sprite.setPosition(M1Position);
 					window.draw(SkySprite2);
 				}
@@ -464,13 +552,52 @@ int main() {
 		}
 		
 
+
+		if (GameOver == true) {
 			window.draw(TextBoxSprite);
 			window.draw(DyingText);
-		if (GameOver == true) {
+			window.draw(DyingResetText);
 
-			if (score > 10 && score < 150) {
-				window.draw(ResetText);
+			if (score > 10 && score < 200) {
+				window.draw(Info1);
 			}
+
+			if (score > 200 && score < 450) {
+				window.draw(Info2);
+			}
+
+			if (score > 450 && score < 850) {
+				window.draw(Info3);
+			}
+
+			if (score > 850 && score < 1250) {
+				window.draw(Info4);
+			}
+
+			if (score > 1250 && score < 1600) {
+				window.draw(Info5);
+			}
+
+			if (score > 1600 && score < 1950) {
+				window.draw(Info6);
+			}
+
+			if (score > 1950 && score < 2300) {
+				window.draw(Info7);
+			}
+
+			if (score > 2300 && score < 2700) {
+				window.draw(Info8);
+			}
+
+			if (score > 2700 && score < 3100) {
+				window.draw(Info9);
+			}
+
+			if (score > 3100) {
+				window.draw(Info10);
+			}
+
 		}
 
 		window.display();
